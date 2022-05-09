dd5ff1e304a98674475190ea3e167ee7cdf3b3fd
Justin Bak <tkdefender@protonmail.com>
2022-05-09 14:54:21 -0600

    cleaned up and added zero

--- app.py
+++ app.py
@@ -8,7 +8,8 @@ from PyQt5.QtWidgets import (
     QPushButton,
     QSlider,
     QFrame,
-    QLineEdit
+    QLineEdit,
+    QLabel
 )
 
 class App(QWidget):
@@ -32,26 +33,54 @@ class App(QWidget):
         self.frame = QFrame(self)
         self.frame.setGeometry(QRect(40, 40, 250, 180))
 
+        self.angleLabel = QLabel('Angle:', parent=self.frame)
+        self.angleLabel.setGeometry(20, 0, 200, 30)
+
         self.slider = QSlider(parent=self.frame)
+        self.slider.setGeometry(QRect(0, 15, 100, 50))
         self.slider.setOrientation(Qt.Horizontal)
         self.slider.setTickInterval(1)
         self.slider.setMinimum(0)
         self.slider.setMaximum(360)
         self.slider.valueChanged.connect(self.slider_changed)
 
+        self.speed_slider = QSlider(parent=self.frame)
+        self.speed_slider.setGeometry(QRect(0, 40, 100, 50))
+        self.speed_slider.setOrientation(Qt.Horizontal)
+        self.speed_slider.setTickInterval(1)
+        self.speed_slider.setMinimum(0)
+        self.speed_slider.setMaximum(100)
+        self.speed_slider.valueChanged.connect(self.speed_slider_changed)
+
         self.textEdit = QLineEdit(self.frame)
-        self.textEdit.setGeometry(QRect(0, 50, 50, 30))
+        self.textEdit.setGeometry(QRect(115, 20, 50, 20))
         self.textEdit.setText(str(0))
         self.textEdit.textChanged.connect(self.textChanged)
         self.textEdit.setValidator(QIntValidator(0, 360, self))
 
-        self.button = QPushButton(parent=self.frame, text="send")
-        self.button.setGeometry(100, 0, 50, 20)
-        self.button.clicked.connect(self.buttonClicked)
+        self.speed_textEdit = QLineEdit(self.frame)
+        self.speed_textEdit.setGeometry(QRect(115, 50, 50, 20))
+        self.speed_textEdit.setText(str(0))
+        self.speed_textEdit.textChanged.connect(self.speed_textChanged)
+        self.speed_textEdit.setValidator(QIntValidator(0, 100, self))
+
+
+
+        self.startButton = QPushButton(parent=self.frame, text="start")
+        self.startButton.setGeometry(120, 90, 50, 20)
+        self.startButton.clicked.connect(self.startButtonClicked)
+
+        self.zeroButton = QPushButton(parent=self.frame, text="zero")
+        self.zeroButton.setGeometry(60, 90, 50, 20)
+        self.zeroButton.clicked.connect(self.zeroButtonClicked)
 
         self.show()
 
-    def buttonClicked(self):
+    def zeroButtonClicked(self):
+        self.vm.zeroArm()
+        print('zero clicked')
+
+    def startButtonClicked(self):
         self.vm.sendAngleStart()
         print('button clicked: ' + str(self.vm.getAngle()))
 
@@ -59,6 +88,12 @@ class App(QWidget):
         value = self.slider.value()
         self.vm.setAngle(value)
         self.textEdit.setText(str(self.vm.getAngle()))
+
+    def speed_slider_changed(self):
+        pass
+
+    def speed_textChanged(self):
+        pass
 
     def textChanged(self):
         value = self.textEdit.text()
--- appVM.py
+++ appVM.py
@@ -5,8 +5,9 @@ class appViewModel():
 
     def __init__(self):
         self.angle = 0
+        self.speed = 100
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        #self.socket.bind(("", appViewModel.port))
+        self.addressPort = ('192.168.1.189', appViewModel.port)
 
     def getAngle(self):
         return self.angle
@@ -14,8 +15,16 @@ class appViewModel():
     def setAngle(self, angle):
         if angle >= 0 and angle <= 360:
             self.angle = angle
+        
+    def zeroArm(self):
+        msg = "{command} {angle} {speed}"
+        msg = msg.format(command=46, angle=0, speed=50)
+        msg = str.encode(msg)
+        self.socket.sendto(msg, self.addressPort)
 
     def sendAngleStart(self):
-        addressPort = ('192.168.1.189', appViewModel.port)
-        msg = str.encode(str(self.angle))
-        self.socket.sendto(msg, addressPort)
+        msg = "{command} {angle} {speed}"
+        msg = msg.format(command=165, angle=self.angle, speed=self.speed)
+        msg = str.encode(msg)
+        print(msg)
+        self.socket.sendto(msg, self.addressPort)
