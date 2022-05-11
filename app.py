94468cdbc78d1c70d306f1c3d130cc304ffca618
Justin Bak <tkdefender@protonmail.com>
2022-05-11 07:43:18 -0600

    ui for pid settings

--- app.py
+++ app.py
@@ -9,7 +9,8 @@ from PyQt5.QtWidgets import (
     QSlider,
     QFrame,
     QLineEdit,
-    QLabel
+    QLabel,
+    QGridLayout
 )
 
 class App(QWidget):
@@ -33,8 +34,11 @@ class App(QWidget):
         self.frame = QFrame(self)
         self.frame.setGeometry(QRect(40, 40, 250, 180))
 
-        self.angleLabel = QLabel('Angle:', parent=self.frame)
-        self.angleLabel.setGeometry(20, 0, 200, 30)
+        self.angleLabel = QLabel(u'\N{DEGREE SIGN} Angle', parent=self.frame)
+        self.angleLabel.setGeometry(180, 25, 200, 30)
+
+        self.speedLabel= QLabel('% Speed', parent=self.frame)
+        self.speedLabel.setGeometry(180, 60, 200, 30)
 
         self.slider = QSlider(parent=self.frame)
         self.slider.setGeometry(QRect(0, 15, 100, 50))
@@ -45,7 +49,7 @@ class App(QWidget):
         self.slider.valueChanged.connect(self.slider_changed)
 
         self.speed_slider = QSlider(parent=self.frame)
-        self.speed_slider.setGeometry(QRect(0, 40, 100, 50))
+        self.speed_slider.setGeometry(QRect(0, 50, 100, 50))
         self.speed_slider.setOrientation(Qt.Horizontal)
         self.speed_slider.setTickInterval(1)
         self.speed_slider.setMinimum(0)
@@ -53,36 +57,69 @@ class App(QWidget):
         self.speed_slider.valueChanged.connect(self.speed_slider_changed)
 
         self.textEdit = QLineEdit(self.frame)
-        self.textEdit.setGeometry(QRect(115, 20, 50, 20))
+        self.textEdit.setGeometry(QRect(115, 30, 50, 20))
         self.textEdit.setText(str(0))
         self.textEdit.textChanged.connect(self.textChanged)
         self.textEdit.setValidator(QIntValidator(0, 360, self))
 
         self.speed_textEdit = QLineEdit(self.frame)
-        self.speed_textEdit.setGeometry(QRect(115, 50, 50, 20))
+        self.speed_textEdit.setGeometry(QRect(115, 65, 50, 20))
         self.speed_textEdit.setText(str(0))
         self.speed_textEdit.textChanged.connect(self.speed_textChanged)
         self.speed_textEdit.setValidator(QIntValidator(0, 100, self))
 
-
-
         self.startButton = QPushButton(parent=self.frame, text="start")
-        self.startButton.setGeometry(120, 90, 50, 20)
+        self.startButton.setGeometry(120, 100, 50, 20)
         self.startButton.clicked.connect(self.startButtonClicked)
 
         self.zeroButton = QPushButton(parent=self.frame, text="zero")
-        self.zeroButton.setGeometry(60, 90, 50, 20)
+        self.zeroButton.setGeometry(60, 100, 50, 20)
         self.zeroButton.clicked.connect(self.zeroButtonClicked)
+
+        self.pidFrame = QFrame(self)
+        self.pidFrame.setGeometry(QRect(300, 40, 200, 160))
+        gridLayout = QGridLayout()
+        self.pidFrame.setLayout(gridLayout)
+
+        self.multLabel= QLabel('Mult')
+        self.multLabel.setGeometry(QRect(0, 0, 50, 20))
+        gridLayout.addWidget(self.multLabel, 0, 1)
+
+        self.divLabel= QLabel('Div')
+        gridLayout.addWidget(self.divLabel, 0, 0)
+
+        self.Ilabel = QLabel('I')
+        gridLayout.addWidget(self.Ilabel, 1, 0, 1, -1)
+
+        self.iDivText = QLineEdit("ID")
+        self.iDivText.setValidator(QIntValidator(0, 100, self))
+        gridLayout.addWidget(self.iDivText, 2, 0)
+
+        self.iMultText = QLineEdit("IM")
+        self.iMultText.setValidator(QIntValidator(0, 100, self))
+        gridLayout.addWidget(self.iMultText, 2, 1)
+
+        self.Ilabel = QLabel('P')
+        gridLayout.addWidget(self.Ilabel, 3, 0, 1, -1)
+
+        self.pDivText = QLineEdit("PD")
+        self.pDivText.setValidator(QIntValidator(0, 100, self))
+        gridLayout.addWidget(self.pDivText, 4, 0)
+
+        self.pMultText = QLineEdit("PM")
+        self.pMultText.setValidator(QIntValidator(0, 100, self))
+        gridLayout.addWidget(self.pMultText, 4, 1)
+
+        self.pidButton = QPushButton(text="set")
+        gridLayout.addWidget(self.pidButton)
 
         self.show()
 
     def zeroButtonClicked(self):
         self.vm.zeroArm()
-        print('zero clicked')
 
     def startButtonClicked(self):
         self.vm.sendAngleStart()
-        print('button clicked: ' + str(self.vm.getAngle()))
 
     def slider_changed(self):
         value = self.slider.value()
@@ -90,9 +127,15 @@ class App(QWidget):
         self.textEdit.setText(str(self.vm.getAngle()))
 
     def speed_slider_changed(self):
-        pass
+        value = self.speed_slider.value()
+        self.vm.setSpeed(value)
+        self.speed_textEdit.setText(str(self.vm.getSpeed()))
 
     def speed_textChanged(self):
+        value = self.speed_textEdit.text()
+        if value != '':
+            self.vm.setSpeed(int(value))
+            self.speed_slider.setValue(self.vm.getSpeed())
         pass
 
     def textChanged(self):
--- appVM.py
+++ appVM.py
@@ -1,11 +1,13 @@
 import socket
 
+ZERO_SPEED = 10
+
 class appViewModel():
     port = 6666
 
     def __init__(self):
         self.angle = 0
-        self.speed = 100
+        self.speed = 0 
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         self.addressPort = ('192.168.1.189', appViewModel.port)
 
@@ -15,10 +17,17 @@ class appViewModel():
     def setAngle(self, angle):
         if angle >= 0 and angle <= 360:
             self.angle = angle
+
+    def setSpeed(self, speed):
+        if speed >= 0 and speed <= 100:
+            self.speed = speed
+    
+    def getSpeed(self):
+        return self.speed
         
     def zeroArm(self):
         msg = "{command} {angle} {speed}"
-        msg = msg.format(command=46, angle=0, speed=50)
+        msg = msg.format(command=46, angle=0, speed=ZERO_SPEED)
         msg = str.encode(msg)
         self.socket.sendto(msg, self.addressPort)
 
@@ -26,5 +35,4 @@ class appViewModel():
         msg = "{command} {angle} {speed}"
         msg = msg.format(command=165, angle=self.angle, speed=self.speed)
         msg = str.encode(msg)
-        print(msg)
         self.socket.sendto(msg, self.addressPort)
