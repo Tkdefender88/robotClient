fd4c151132398f7ec62d09876a2b653751985863
Justin Bak <tkdefender@protonmail.com>
2022-05-11 15:22:17 -0600

    send pid message

--- appVM.py
+++ appVM.py
@@ -8,6 +8,12 @@ class appViewModel():
     def __init__(self):
         self.angle = 0
         self.speed = 0 
+        self.pMult = 0
+        self.pDiv  = 0
+        self.iMult = 0
+        self.iDiv  = 0
+        self.dMult = 0
+        self.dDiv  = 0
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         self.addressPort = ('192.168.1.189', appViewModel.port)
 
@@ -35,4 +41,17 @@ class appViewModel():
         msg = "{command} {angle} {speed}"
         msg = msg.format(command=165, angle=self.angle, speed=self.speed)
         msg = str.encode(msg)
+        self.socket.sendto(msg, self.addressPort)
+
+    def setPID(self):
+        msg = "{command} {pm} {pd} {im} {id} {dm} {dd}"
+        msg = msg.format(
+            command=5,
+            pm=self.pMult,
+            pd=self.pDiv,
+            im=self.iMult,
+            id=self.iDiv,
+            dm=self.dMult,
+            dd=self.dDiv
+        )
         self.socket.sendto(msg, self.addressPort)
